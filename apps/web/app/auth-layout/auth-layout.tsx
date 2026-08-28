2d358e52d7f28ccb9b5c9b453076f5653d7660c4
IvanKharkivskyi1 <ikcomua@gmail.com>
2026-08-28 17:41:48 +0300

    updated image

--- apps/web/app/auth-layout/auth-layout.tsx
+++ apps/web/app/auth-layout/auth-layout.tsx
@@ -26,7 +26,7 @@ export function AuthLayout({
         <Card.Header>
           <VStack gap="3" align="center">
             <Image
-              src="https://iynb6msbiue214kh.private.blob.vercel-storage.com/logo/logo.webp?vercel-blob-valid-until=1787926371009&vercel-blob-delegation=eyJzdG9yZUlkIjoic3RvcmVfaXluQjZtc0JJVUUyMTRraCIsIm93bmVySWQiOiJ0ZWFtX2ZWSVJzcjRrUG9KaHJubWtCaHg5dW42RiIsInBhdGhuYW1lIjoiKiIsIm9wZXJhdGlvbnMiOlsiZ2V0IiwiaGVhZCJdLCJ2YWxpZFVudGlsIjoxNzg3OTY5NDM2NjkxLCJpYXQiOjE3ODc5MjYyMzU4MTV9.GtqXlCMePIUu2Fuf3dlL4XGqKyObXevJT9dyRMra__M&vercel-blob-signature=tZmbCpuVEYGQQfOF8gWV1vKTAqHuAUdWseinCD1rXTY"
+              src="https://iynb6msbiue214kh.private.blob.vercel-storage.com/logo/logo.webp?vercel-blob-valid-until=1787928116294&vercel-blob-delegation=eyJzdG9yZUlkIjoic3RvcmVfaXluQjZtc0JJVUUyMTRraCIsIm93bmVySWQiOiJ0ZWFtX2ZWSVJzcjRrUG9KaHJubWtCaHg5dW42RiIsInBhdGhuYW1lIjoiKiIsIm9wZXJhdGlvbnMiOlsiZ2V0IiwiaGVhZCJdLCJ2YWxpZFVudGlsIjoxNzg3OTcxMjUyODY0LCJpYXQiOjE3ODc5MjgwNTE5ODJ9.YABLtbF6U_WJww2LRJWKGRtPyixl29D33EfYqUzKjyE&vercel-blob-signature=-WUtb6tobE4g5KGgZTs4Qejp00i15rH6dwYi2sTq444"
               alt="Logo"
               width={48}
               height={48}
